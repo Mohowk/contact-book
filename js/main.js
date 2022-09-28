0ae454d8035f76bec9f6e1e4d4305d35802920f3
Mohowk <azaboyjoy@gmail.com>
2022-09-28 18:57:55 +0600

    Crud Application

--- js/main.js
+++ js/main.js
@@ -1,47 +1,75 @@
-var selectedRow = null;
+let selectedRow = null;
 
 // Show Alerts
 
-function showAlert(message, className){
+function showAlert(message, className) {
   const div = document.createElement("div");
-  div.className = `alert alert-${className}`; 
+  div.className = `alert alert-${className}`;
 
-  div.appendChild(document.creatTextNode(message));
+  div.appendChild(document.createTextNode(message));
   const container = document.querySelector(".container");
   const main = document.querySelector("div, main");
-  container.insertBefore(div, main);
+  container.after(div, main);
 
-  setTimeout(()=> document.querySelector(".alert").remove(),3000);
+  setTimeout(() => document.querySelector(".alert").remove(), 1000);
 }
 
 // Clear All Fields
-function  clearFields(){
+function clearFields() {
   document.querySelector("#firstName").value = "";
   document.querySelector("#LastName").value = "";
   document.querySelector("#rollNo").value = "";
 }
 
-// Add Data 
+function readData() {
+  if (!localStorage.getItem("users")) {
+    localStorage.setItem("users", "[]");
+  }
+  let data = JSON.parse(localStorage.getItem("users"));
+  data.forEach((item, index) => {
+    const list = document.querySelector("#student-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${item.firstName}</td>
+            <td>${item.lastName}</td>
+            <td>${item.rollNo}</td>
+            <td>
+            <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+            <a href="#" class="btn btn-danger btn-sm delete" id="${index}">Delete</a>
+        `;
+    list.appendChild(row);
+  });
+}
 
-document.querySelector("#student-form").addEventListener("submit",(e)=>{
+// Add Data
+
+document.querySelector("#student-form").addEventListener("submit", e => {
   e.preventDefault();
 
-  // Get Form Values 
+  // Get Form Values
   const firstName = document.querySelector("#firstName").value;
-  const lastName = document.querySelector("#lastName").value;
-  const rollNo = document.querySelector("#rollName").value;
-
+  const lastName = document.querySelector("#LastName").value;
+  const rollNo = document.querySelector("#rollNo").value;
 
   // Validate
-  
-  if (firstName == "" || lastName == "" || rollNo == ""){
-    showAlert("Please fill in all fields", "danger");
-  }
-  else{
-    if(selectedRow == null){
-      const list = document.querySelector("#student-list");
-      const row = document.creatElement("tr");
 
+  if (firstName == "" || lastName == "" || rollNo == "") {
+    showAlert("Please fill in all fields", "danger");
+  } else {
+    if (selectedRow == null) {
+      const list = document.querySelector("#student-list");
+      const row = document.createElement("tr");
+      if (!localStorage.getItem("users")) {
+        localStorage.setItem("users", "[]");
+      }
+      let data = JSON.parse(localStorage.getItem("users"));
+      let newObj = {
+        firstName: firstName,
+        lastName: lastName,
+        rollNo: rollNo,
+      };
+      data.push(newObj);
+      localStorage.setItem("users", JSON.stringify(data));
       row.innerHTML = `
           <td>${firstName}</td>
           <td>${lastName}</td>
@@ -52,7 +80,7 @@ document.querySelector("#student-form").addEventListener("submit",(e)=>{
       `;
       list.appendChild(row);
       selectedRow = null;
-      showAlert("Student Added","success");
+      showAlert("Student Added", "success");
     }
     // else{
     //   selectedRow.children[0].textContent = firstName;
@@ -64,28 +92,37 @@ document.querySelector("#student-form").addEventListener("submit",(e)=>{
 
     clearFields();
   }
-  
 });
 
 // Edit Data
 
-document.querySelector("#Student-list").addEventListener("click",(e)=>{
+document.querySelector("#student-list").addEventListener("click", e => {
   target = e.target;
-  if(target.classList.contains("edit")){
+  if (target.classList.contains("edit")) {
     selectedRow = target.parentElement.parentElement;
-    document.querySelector("#firstName").value = selectedRow.children[0].textContent;
-    document.querySelector("#lastName").value = selectedRow.children[1].textContent;
-    document.querySelector("#rollNo").value = selectedRow.children[2].textContent;
+    document.querySelector("#firstName").value =
+      selectedRow.children[0].textContent;
+    document.querySelector("#lastName").value =
+      selectedRow.children[1].textContent;
+    document.querySelector("#rollNo").value =
+      selectedRow.children[2].textContent;
   }
 });
 
-
 // Delete Data
 
-document.querySelector("#student-list").addEventListener("click",(e)=>{
-    target = e.target;
-    if(target.classList.contains("delete")){
-      target.parentElement.parentElement.remove();
-      showAlert("Student Data Delete","danger");
-    }
+document.querySelector("#student-list").addEventListener("click", e => {
+  target = e.target;
+  if (target.classList.contains("delete")) {
+    target.parentElement.parentElement.remove();
+    showAlert("Student Data Delete", "danger");
+  }
+  if (!localStorage.getItem("users")) {
+    localStorage.setItem("users", "[]");
+  }
+  let data = JSON.parse(localStorage.getItem("users"));
+  data.splice(e.target.id, 1);
+  localStorage.setItem("users", JSON.stringify(data));
 });
+
+readData();
